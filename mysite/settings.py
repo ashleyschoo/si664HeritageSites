d45b7c8f626f615bd985e72eb61527accfed3e32
Ashley Schoonover <aschoo@umich.edu>
2018-12-04 00:03:49 -0500

    fixed keys

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aq0qj6yp016brwv6tu^armh3smi7-9bn--4y$0u8y6#v#!5rn4'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
